afba99cdea64cceb26c9cd7f10f1ac47ddc8bdaf
Alexis Parave <aparavev@gmail.com>
2022-10-21 19:46:27 -0500

    Add callbacks for OrderLine model

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,4 +1,25 @@
 class OrderLine < ApplicationRecord
+  # Associations
   belongs_to :order
   belongs_to :product
+
+  # Callbacks
+  before_create :set_price
+  before_create :calculate_total
+  after_create :update_order_total
+
+  private
+
+  def set_price
+    self.price = product.price
+  end
+
+  def calculate_total
+    self.total = quantity * price
+  end
+
+  def update_order_total
+    order.total += total
+    order.save
+  end
 end
