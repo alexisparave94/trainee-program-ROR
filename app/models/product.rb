c9dc9f8331566ee317f28d30b4265841a559addc
Alexis Parave <aparavev@gmail.com>
2022-10-22 23:35:56 -0500

    Add scope for Product model to select what is the must purchased product

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ApplicationRecord
   # Scopes
   default_scope { order(:name) }
   scope :order_by_price, -> { order(:price) }
+  scope :get_most_purchased_product, -> { joins(order_lines: :order).where('orders.status': 1).group(:id).order('SUM(order_lines.quantity) DESC').limit(1) }
 end
