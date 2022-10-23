65322766c1de5340290ad018ee86452f58f49ad0
Alexis Parave <aparavev@gmail.com>
2022-10-22 23:39:27 -0500

    Add scop for Product model to search product which name contains words greater than two letters

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,10 @@ class Product < ApplicationRecord
   default_scope { order(:name) }
   scope :order_by_price, -> { order(:price) }
   scope :get_most_purchased_product, -> { joins(order_lines: :order).where('orders.status': 1).group(:id).order('SUM(order_lines.quantity) DESC').limit(1) }
+
+  def self.get_products_which_name_has_words_greater_than_2_letters
+    Product.all.select do |product|
+      product.name.split(' ').any? { |word| word.length > 2 }
+    end
+  end
 end
