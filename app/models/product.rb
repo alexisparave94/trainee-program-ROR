0d0fb4bfa7b93fbf6d1238ef4e359e1db1e4eb59
Alexis Parave <aparavev@gmail.com>
2022-10-23 00:13:05 -0500

    Add validations for Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,4 +13,13 @@ class Product < ApplicationRecord
       product.name.split(' ').any? { |word| word.length > 2 }
     end
   end
+
+  # Validations
+  validates :name, presence: { message: 'Must enter a name' }
+  validates :name, uniqueness: { message: 'Name "%{value}" already exists' }
+  validates :sku, presence: { message: 'Must enter a sku' }
+  validates :sku, uniqueness: { message: 'Sku "%{value}" already exists' }
+  validates :stock, numericality: { only_integer: true, message: 'Must be an integer' }
+  validates :stock, numericality: { greater_than_or_equal_to: 0, message: 'Must be a positive number' }
+  validates :price, numericality: { greater_than: 0, message: 'Must be a positive number greater than 0' }
 end
