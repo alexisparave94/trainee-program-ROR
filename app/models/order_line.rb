68f49a4312cc495c584d38e08172285b93e05b37
Alexis Parave <aparavev@gmail.com>
2022-10-23 00:30:00 -0500

    Add validations for OrderLine model

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -8,6 +8,11 @@ class OrderLine < ApplicationRecord
   before_create :calculate_total
   after_create :update_order_total
 
+  # Validations
+  validates :product, uniqueness: { scope: :order, message: "This product already is include in the order" }
+  validates :quantity, numericality: { only_integer: true, message: 'Must be an integer' }
+  validates :quantity, numericality: { greater_than_or_equal_to: 0, message: 'Must be a positive number' }
+
   private
 
   def set_price
