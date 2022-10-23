82d14c6b49c48221a2d40ed02102447fbc0b5c3f
Alexis Parave <aparavev@gmail.com>
2022-10-22 23:27:24 -0500

    Add default scope for Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   # Associations
   has_many :order_lines, dependent: :destroy
   has_many :orders, through: :order_lines
+
+  # Scopes
+  default_scope { order(:name) }
 end
