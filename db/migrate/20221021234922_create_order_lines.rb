204e009b607a0f08b9cfb4d15278ea754d44ae47
Alexis Parave <aparavev@gmail.com>
2022-10-27 01:35:15 -0500

    :card_file_box: Update order_lines default values

--- db/migrate/20221021234922_create_order_lines.rb
+++ db/migrate/20221021234922_create_order_lines.rb
@@ -1,9 +1,9 @@
 class CreateOrderLines < ActiveRecord::Migration[7.0]
   def change
     create_table :order_lines do |t|
-      t.references :order, null: false, foreign_key: true
+      t.references :order, null: true, foreign_key: true
       t.references :product, null: false, foreign_key: true
-      t.integer :quantity
+      t.integer :quantity, default: 1
       t.decimal :price
       t.decimal :total
 
