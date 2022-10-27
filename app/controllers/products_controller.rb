63541abc45b53227aeb4d245bb5379ea08065987
Alexis Parave <aparavev@gmail.com>
2022-10-26 22:58:41 -0500

    :sparkles: Add functionaility to create a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,23 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: 'Product create successfully'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:sku, :name, :description,:price, :stock)
+  end
 end
