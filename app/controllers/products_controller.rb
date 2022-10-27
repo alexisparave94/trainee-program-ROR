e7569be7832aad74f91dff9f578f1e2f9145d01a
Alexis Parave <aparavev@gmail.com>
2022-10-26 23:09:42 -0500

    :sparkles: Add functionaility to update a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,22 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to products_path, notice: 'Product create successfully'
+      redirect_to products_path, notice: 'Product was successfully created'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path, notice: 'Product was successfully updated'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
