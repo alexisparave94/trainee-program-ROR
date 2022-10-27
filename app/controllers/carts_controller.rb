e61e650e92d5e87ccd916f1c52fa1c59b2ed4de6
Alexis Parave <aparavev@gmail.com>
2022-10-27 14:11:47 -0500

    :sparkles: Add functionality to empty the current shopping cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,17 @@
 class CartsController < ApplicationController
-  def show
+  before_action :set_cart
+
+  def show; end
+
+  def destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
+    redirect_to products_path, notice: 'Your cart has been emptied'
+  end
+
+  private
+
+  def set_cart
     @cart = Cart.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources :order_lines, only: %i[new create edit update destroy]
 
-  resources :carts, only: %i[show]
+  resources :carts, only: %i[show destroy]
 end
