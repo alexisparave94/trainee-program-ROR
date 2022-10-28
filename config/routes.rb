177b968dd5ace3f640e990ad7bff9cecf9df39dd
Alexis Parave <aparavev@gmail.com>
2022-10-28 14:23:26 -0500

    :sparkles: Update routes to work with admin namesapace

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
 
-  resources :products
+  resources :products, only: %i[index show]
   get 'search/' => 'products#search_product'
 
   resources :order_lines, only: %i[new create edit update destroy]
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   resources :orders, only: %i[show create destroy]
   get 'show_cart' => 'orders#show_cart'
   get 'checkout' => 'orders#checkout'
+
+  namespace :admin do
+    resources :products, only: %i[new create edit update destroy]
+  end
 end
