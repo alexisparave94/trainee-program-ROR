b5c81a13c88bfcd42b42bd5b2cc90933de317cf4
Alexis Parave <aparavev@gmail.com>
2022-10-29 03:33:47 -0500

    :passport_control: Add method to rescue unauthorized actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :load_cart
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def load_cart
     return unless user_signed_in?
 
@@ -24,5 +26,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[firt_name last_name])
     devise_parameter_sanitizer.permit(:sign_in, keys: %i[firt_name last_name])
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to(request.referrer || root_path)
   end
 end
