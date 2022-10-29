378d8bb607189458ac47a332cfd3c34b0c8d5db1
Alexis Parave <aparavev@gmail.com>
2022-10-29 04:12:11 -0500

    :passport_control: Add order policies

---
+++ app/policies/order_policy.rb
@@ -0,0 +1,12 @@
+class OrderPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def update?
+    user&.customer?
+  end
+end
