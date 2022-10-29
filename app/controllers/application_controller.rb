7b275560840113e594309d74cb8f152dc0e16db8
Alexis Parave <aparavev@gmail.com>
2022-10-29 02:07:58 -0500

    :sparkles: Add first and last name to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :load_cart
 
   def load_cart
@@ -15,5 +16,12 @@ class ApplicationController < ActionController::Base
         total: @order.calculate_total
       )
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firt_name last_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[firt_name last_name])
   end
 end
