7bebb35e3cdf722c5932f52f6d4c3fa0dd3bfc47
Alexis Parave <aparavev@gmail.com>
2022-10-29 17:30:57 -0500

    :sparkles: Change column product of change logs table

--- db/migrate/20221029191517_create_change_logs.rb
+++ db/migrate/20221029191517_create_change_logs.rb
@@ -2,6 +2,7 @@ class CreateChangeLogs < ActiveRecord::Migration[7.0]
   def change
     create_table :change_logs do |t|
       t.string :description
+      t.string :product
       t.references :user, null: true, foreign_key: true
 
       t.timestamps
