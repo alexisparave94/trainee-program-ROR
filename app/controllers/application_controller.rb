82a3480a834138c0e48314ecdca2eabd05e27bd6
Alexis Parave <aparavev@gmail.com>
2022-10-29 01:27:55 -0500

    :sparkles: Add functionaility to add a shopping cart to the user that is loged in or to recover a shipping card pending of the current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :load_cart
+
+  def load_cart
+    return unless user_signed_in?
+
+    @pending_order = current_user.orders.where(status: 'pending').take
+    if @pending_order
+      session[:order_id] = @pending_order.id
+    elsif session[:order_id]
+      @order = Order.find(session[:order_id])
+      @order.update(
+        status: 'pending',
+        user: current_user,
+        total: @order.calculate_total
+      )
+    end
+  end
 end
