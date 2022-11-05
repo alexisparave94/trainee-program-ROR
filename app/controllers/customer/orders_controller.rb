9e6b34aba24706a2c2d74bc267eb43052cb6e339
Alexis Parave <aparavev@gmail.com>
2022-11-05 14:28:00 -0500

    :art: Add style module children

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,31 +1,33 @@
 # frozen_string_literal: true
 
-# Class to manage Orders Controller of the namespace from customer
-class Customer::OrdersController < ApplicationController
-  before_action :authenticate_user!, only: %i[update]
+module Customer
+  # Class to manage Orders Controller of the namespace from customer
+  class OrdersController < ApplicationController
+    before_action :authenticate_user!, only: %i[update]
 
-  def index
-    @orders = current_user.orders.where(status: 'completed').order(created_at: :DESC)
-    authorize Order
-  end
+    def index
+      @orders = current_user.orders.where(status: 'completed').order(created_at: :DESC)
+      authorize Order
+    end
 
-  def show
-    @commentable = Order.find(params[:id])
-    @rate = current_user.get_last_rate(@commentable)
-    @comment = Comment.new(rate: @rate)
-    authorize @commentable
-  end
+    def show
+      @commentable = Order.find(params[:id])
+      @rate = current_user.get_last_rate(@commentable)
+      @comment = Comment.new(rate: @rate)
+      authorize @commentable
+    end
 
-  def update
-    @order = Order.find(params[:id])
-    authorize @order
-    if @order.update(status: 'completed', total: @order.calculate_total)
-      @order.update_products_stock
-      session[:order_id] = nil
-      session[:checkout] = nil
-      redirect_to products_path, notice: 'Thanks for buy'
-    else
-      render :new, status: :unprocessable_entity
+    def update
+      @order = Order.find(params[:id])
+      authorize @order
+      if @order.update(status: 'completed', total: @order.calculate_total)
+        @order.update_products_stock
+        session[:order_id] = nil
+        session[:checkout] = nil
+        redirect_to products_path, notice: 'Thanks for buy'
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 end
--- app/models/poro/sort.rb
+++ app/models/poro/sort.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-# Class to manage Sort PORO
-class Poro::Sort
-  attr_reader :id, :name
+module Poro
+  # Class to manage Sort PORO
+  class Sort
+    attr_reader :id, :name
 
-  def initialize(id, name)
-    @id = id
-    @name = name
-  end
+    def initialize(id, name)
+      @id = id
+      @name = name
+    end
 
-  def self.sort_options
-    [
-      Poro::Sort.new('ASC', 'A-Z'),
-      Poro::Sort.new('DESC', 'Z-A'),
-      Poro::Sort.new('like', 'Likes')
-    ]
+    def self.sort_options
+      [
+        Poro::Sort.new('ASC', 'A-Z'),
+        Poro::Sort.new('DESC', 'Z-A'),
+        Poro::Sort.new('like', 'Likes')
+      ]
+    end
   end
 end
