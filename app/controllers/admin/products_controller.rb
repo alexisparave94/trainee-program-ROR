4b8899c0aeeb30f4c137c10a57a05d0a102c09d5
Alexis Parave <aparavev@gmail.com>
2022-11-05 04:31:35 -0500

    :recycle: Update funtionality for change log

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -30,9 +30,9 @@ module Admin
 
     # PATCH /admin/products/:id
     def update
-      authorize @product
+      # authorize @product
+      User.set_user(current_user)
       if @product.update(product_params)
-        save_change_log(request.request_method)
         redirect_to products_path, notice: 'Product was successfully updated'
       else
         render :edit, status: :unprocessable_entity
