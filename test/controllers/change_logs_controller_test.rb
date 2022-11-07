3d6c612cdeb2a56605365831406b4b62fef50aa4
Alexis Parave <aparavev@gmail.com>
2022-11-06 20:10:39 -0500

    :recycle: Update tests for change logs controller

--- test/controllers/change_logs_controller_test.rb
+++ test/controllers/change_logs_controller_test.rb
@@ -4,18 +4,14 @@ require 'test_helper'
 
 class ChangeLogsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    user = create(:user, role: 'admin')
-
-    post new_user_session_url, params: { user: { email: user.email, password: user.password } }
+    sign_in create(:user, role: 'admin')
 
     get admin_change_logs_url
     assert_response :success
   end
 
   test 'should report not authorized to get index' do
-    user = create(:user)
-
-    post new_user_session_url, params: { user: { email: user.email, password: user.password } }
+    sign_in create(:user)
 
     get admin_change_logs_url
 
