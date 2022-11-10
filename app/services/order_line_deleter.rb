ae768702589fb7ca3536755577ce52befc073f4e
Alexis Parave <aparavev@gmail.com>
2022-11-10 03:52:48 -0500

    :recycle:Update destroy method for orderline for no logged in user to work with service object

---
+++ app/services/order_line_deleter.rb
@@ -0,0 +1,16 @@
+class OrderLineDeleter < ApplicationService
+  def initialize(id_order_line, virtual_order)
+    @id_order_line = id_order_line
+    @virtual_order = virtual_order
+  end
+  
+  def call
+    delete_virtual_order_line
+  end
+
+  private
+
+  def delete_virtual_order_line
+    @virtual_order.reject { |line| line['id'] == @id_order_line.to_i }
+  end
+end
