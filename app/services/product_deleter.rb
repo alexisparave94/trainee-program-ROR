c47e41fb5c572a071f2b6ccd20569f43f795ff00
Alexis Parave <aparavev@gmail.com>
2022-11-10 01:16:29 -0500

    :recycle: Update destroy to work with Product Deleter service object

---
+++ app/services/product_deleter.rb
@@ -0,0 +1,15 @@
+class ProductDeleter < ApplicationService
+  def initialize(product)
+    @product = product
+  end
+  
+  def call
+    delete_product
+  end
+
+  private
+
+  def delete_product
+    @product.destroy
+  end
+end
