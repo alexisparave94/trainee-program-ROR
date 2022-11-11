bfacc385ccf4ce615e04488595445b5a26bed2ed
Alexis Parave <aparavev@gmail.com>
2022-11-10 19:33:07 -0500

    :sparkles: Add functionality to add tags to products

---
+++ app/services/admin/tag_adder.rb
@@ -0,0 +1,19 @@
+module Admin
+  class TagAdder < ApplicationService
+    def initialize(tag_id, product)
+      @tag_id = tag_id
+      @product = product
+      super()
+    end
+
+    def call
+      add_tag
+    end
+
+    private
+
+    def add_tag
+      @product.tags << Tag.find(@tag_id)
+    end
+  end
+end
