3626dcab24b197afd4437a44f7285decfdf64d8b
Alexis Parave <aparavev@gmail.com>
2022-11-13 05:00:39 -0500

    :recycle: Update fucntionality to edit a prodcut to work with service object Product Updater

--- app/models/forms/product_form.rb
+++ app/models/forms/product_form.rb
@@ -5,7 +5,7 @@ module Forms
   class ProductForm
     include ActiveModel::Model
 
-    attr_accessor :name, :sku, :description, :stock, :price
+    attr_accessor :name, :sku, :description, :stock, :price, :product
 
     # Validations
     validates :name, presence: { message: 'Must enter a name' }
@@ -16,38 +16,16 @@ module Forms
     validates :stock, numericality: { greater_than_or_equal_to: 0, message: 'Must be a positive number' }
     validates :price, numericality: { greater_than: 0, message: 'Must be a positive number greater than 0' }
 
-    def initialize(attr = {}, current_user = nil)
-      @current_user = current_user
+    def initialize(attr = {})
       if attr[:id].nil?
         super(attr)
       else
         @product = Product.find(attr[:id])
-        set_attributes
       end
-    end
-
-    def persisted?
-      !@product.nil?
     end
 
     def id
       @product.nil? ? nil : @product.id
-    end
-
-    def update
-      return false unless valid?
-
-      @product.update(name:, sku:, description:, stock:, price:)
-    end
-
-    private
-
-    def set_attributes
-      self.name = attr[:name].nil? ? @product.name : attr[:name]
-      self.sku = attr[:sku].nil? ? @product.sku : attr[:sku]
-      self.description = attr[:description].nil? ? @product.description : attr[:description]
-      self.price = attr[:price].nil? ? @product.price : attr[:price]
-      self.stock = attr[:stock].nil? ? @product.stock : attr[:stock]
     end
   end
 end
