60881f408786b39828dcd7b91aa6d30ff5ee679a
Alexis Parave <aparavev@gmail.com>
2022-11-13 00:59:07 -0500

    :recycle: Update fucntionality to work with service object Comment Product Creator

---
+++ app/controllers/customer/comment_products_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Customer
+  # Class to manage interactions between logged in customer users and comments
+  class CommentProductsController < ApplicationController
+    before_action :set_product
+
+    # Method to add a new comment
+    # - POST /customer/comment_products
+    def create
+      Customer::Comments::CommentProductCreator.new(current_user, comment_product_form_params).call
+      flash[:notice] = 'Comment was successfully added'
+    rescue StandardError => e
+      flash[:error] = e
+    ensure
+      redirect_to @product
+    end
+
+    private
+
+    # Method to set strong params for comment product
+    def comment_product_form_params
+      params.require(:forms_comment_product_form).permit(:description, :rate_value, :product_id)
+    end
+
+    def set_product
+      @product = Product.find(comment_product_form_params[:product_id])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     resources :likes, only: %i[create destroy]
     resources :order_lines, only: %i[destroy]
     resources :order_line_forms, only: %i[new create edit update]
-    resources :comment_product_forms, only: %i[create]
+    resources :comment_products, only: %i[create]
     resources :comment_order_forms, only: %i[create]
     get 'empty_cart' => 'shopping_cart#empty_cart'
     get 'checkout' => 'shopping_cart#checkout'
