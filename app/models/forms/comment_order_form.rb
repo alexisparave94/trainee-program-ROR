73ad58213b8ca91c94bf9fadb04884de13ff8b74
Alexis Parave <aparavev@gmail.com>
2022-11-13 01:25:07 -0500

    :recycle: Update fucntionality to work with service object Comment Order Creator

--- app/models/forms/comment_order_form.rb
+++ app/models/forms/comment_order_form.rb
@@ -5,28 +5,17 @@ module Forms
   class CommentOrderForm
     include ActiveModel::Model
 
-    attr_accessor :description, :rate_value, :order_id, :current_user, :commentable, :rate
+    attr_accessor :description, :rate_value, :order_id, :user, :commentable, :rate
 
     # Validations
     validates :description, presence: { message: 'Must enter a description' }
     validates :rate_value, numericality: { in: 1..10, message: 'Rate must be between 1 and 10' }, allow_blank: true
 
-    def initialize(attr = {}, current_user = nil)
+    def initialize(attr = {}, user = nil)
       super(attr)
-      @current_user = current_user
+      @user = user
       @commentable = Order.find(attr[:order_id]) if attr[:order_id]
-      @rate = RateCommentSetter.call(@current_user, @commentable)
-    end
-
-    def create
-      return false unless valid?
-
-      if @rate
-        @rate.update(value: rate_value) unless rate_value.empty?
-      else
-        @rate = Rate.create(value: rate_value, user: current_user, rateable: @commentable)
-      end
-      @comment = Comment.create(description:, user: current_user, commentable: @commentable, status: 'approved')
+      @rate = RateCommentSetter.call(@user, @commentable)
     end
   end
 end
---
+++ app/services/customer/comments/comment_order_creator.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Customer
+  module Comments
+    # Service object to like a product
+    class CommentOrderCreator < ApplicationService
+      class NotValidEntryRecord < StandardError; end
+
+      def initialize(user, params)
+        @user = user
+        @params = params
+        @commentable = Order.find(params[:order_id])
+        @rate = RateCommentSetter.call(@user, @commentable)
+        super()
+      end
+
+      def call
+        @comment_order_form = Forms::CommentOrderForm.new(@params)
+        raise(NotValidEntryRecord, parse_errors) unless @comment_order_form.valid?
+
+        if @rate
+          @rate.update(value: @comment_order_form.rate_value) unless @comment_order_form.rate_value.empty?
+        else
+          Rate.create(value: @comment_order_form.rate_value, user: @user, rateable: @commentable)
+        end
+        @comment = Comment.create(description: @comment_order_form.description, user: @user, commentable: @commentable)
+      end
+
+      private
+
+      def parse_errors
+        @comment_order_form.errors.messages.map { |_key, error| error }.join(', ')
+      end
+    end
+  end
+end
