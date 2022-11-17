46dfe579f1d98bb606fb2be3622964e0054c5c0e
Alexis Parave <aparavev@gmail.com>
2022-11-17 04:19:47 -0500

    :sparkles: Add api controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Class parent of the api controllers
+class ApiController < ActionController::API
+  include Pundit::Authorization
+  include ErrorHandler
+
+  def not_found
+    render json: { error: 'not_found' }
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split.last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+end
