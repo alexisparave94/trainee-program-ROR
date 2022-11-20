e499fb2efb5324884bfd3985fd385014e9936dfe
Alexis Parave <aparavev@gmail.com>
2022-11-19 21:50:57 -0500

    :recycle: Add query for complex logic to show empty order

--- app/helpers/shopping_cart_helper.rb
+++ app/helpers/shopping_cart_helper.rb
@@ -2,4 +2,7 @@
 
 # Module for Shopping Cart Helpers
 module ShoppingCartHelper
+  def show_empty_messsage?(virtual_order, current_user, order)
+    (virtual_order.nil? && current_user.nil?) || (current_user && order.order_lines.empty?)
+  end
 end
