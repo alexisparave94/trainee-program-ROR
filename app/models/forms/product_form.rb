43bb1b1515363455c23990ba6948efb753afac02
Alexis Parave <aparavev@gmail.com>
2022-11-19 19:37:41 -0500

    :recycle: Update validation for uniq name and sku

--- app/models/forms/product_form.rb
+++ app/models/forms/product_form.rb
@@ -8,12 +8,26 @@ module Forms
     attr_accessor :name, :sku, :description, :stock, :price, :product, :image
 
     # Validations
-    validates :name, presence: { message: 'Must enter a name' }
     validates :sku, presence: { message: 'Must enter a sku' }
-    validates :stock, numericality: { only_integer: true, message: 'Must be an integer' }
-    validates :stock, numericality: { greater_than_or_equal_to: 0, message: 'Must be a positive number' }
-    validates :price, numericality: { greater_than: 0, message: 'Must be a positive number greater than 0' },
+    validate :uniq_sku
+    validates :name, presence: { message: 'Must enter a name' }
+    validate :uniq_name
+    validates :stock, numericality: { only_integer: true, message: ' Stock must be an integer' }
+    validates :stock, numericality: { greater_than_or_equal_to: 0, message: 'Stock must be a positive number' }
+    validates :price, numericality: { greater_than: 0, message: 'Price must be a positive number greater than 0' },
                       allow_nil: true
+
+    def uniq_name
+      return unless Product.find_by(name:)
+
+      errors.add(:name, 'Name has been already taken')
+    end
+
+    def uniq_sku
+      return unless Product.find_by(sku:)
+
+      errors.add(:sku, 'SKU has been already taken')
+    end
 
     def initialize(attr = {})
       if attr[:id].nil?
