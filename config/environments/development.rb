3b2c6a80560334e35f9c61861c36d258036736db
Alexis Parave <aparavev@gmail.com>
2022-12-03 18:17:19 -0500

    :sparkles: Add host produce with ngrok to could work with webhooks since development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "2057-190-237-162-92.sa.ngrok.io"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
